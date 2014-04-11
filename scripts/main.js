2e82e4071b41cf2852de44ecc2e8c2a881d3a566
Jeremy Marquis <jeremy@jeremymarquis.com>
2014-04-11 11:04:45 -0400

    template.conf bugfix, ImageLoader initialization

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,17 @@
 Y.use("node", function (Y) {
 	Y.on("domready", function () {
 
+		// initialize all images
+		new Y.Squarespace.Loader({
+			img: Y.all("img[data-image]:not([data-load])")
+		});
 
+		// refresh all images on window resize
+		Y.on("windowresize", function () {
+			Y.all("img[data-image][src]").each(function (img) {
+				ImageLoader.load(img);
+			});
+		});
 
 	});
 });
